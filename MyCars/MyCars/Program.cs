b43eea7d4fdc03aae0bfa7b640bfae8944c1e1ba
Johnny Charvet <43849966+JoCharvet@users.noreply.github.com>
2023-10-05 11:58:23 +0200

    ajout de la methode Enregistrer voitures

--- MyCars/MyCars/Program.cs
+++ MyCars/MyCars/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace MyCars
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+namespace MyCars
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //List<Voiture> voitures= 
             Console.WriteLine("Hello, World!");
+
+            
+
+        }
+
+        public void EnregistrerVoitures(List<Voiture> voitures)
+        {
+            string fileName = "listeVoitures.json";
+            string jsonString = JsonSerializer.Serialize(voitures);
+            File.WriteAllText(fileName, jsonString);
         }
     }
 }
